a11875ec7a5ea98fb27f8d8530725e7d3ecf1ada
OrangeFire5 <146310194+OrangeFire5@users.noreply.github.com>
2023-10-30 16:50:15 -0600

    Se agrega contenedor de Imagen
    Se genero el marco que contiene a las images

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from clases.ClasesUI import *
 
 app = tk.Tk()
 app.title("Sargasse Vision Analyzer")
 app.geometry("800x500")
-
+frame1 = FrmImagen(app).grid(row=0, column=0, sticky="nsew",padx=10,pady=10)
+frame2 = FrmImagen(app).grid(row=0, column=1, sticky="nsew",padx=10,pady=10)
 
 app.mainloop()
