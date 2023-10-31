d3631886f67c66340a2ddce3a9168912ec338658
OrangeFire5 <146310194+OrangeFire5@users.noreply.github.com>
2023-10-31 16:15:06 -0600

    Actualizacion de main
    Se implementa la clase FrmDatos en el archivo principal
    y se modifican las filas y columnas

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 from clases.frmImagen import *
+from clases.frmDatos import *
 
 app = tk.Tk()
 app.title("Sargasse Vision Analyzer")
 app.geometry("800x500")
 
-app.rowconfigure(0,weight=1)
-app.columnconfigure(0,weight=1)
+app.rowconfigure(0,weight=3)
+app.rowconfigure(1,weight=1)
+app.columnconfigure(0,weight=3)
 app.columnconfigure(1,weight=1)
+app.columnconfigure(2,weight=3)
+
 
 frame1 = FrmImagen(app).grid(row=0, column=0, sticky="nsew",padx=10,pady=10)
-frame2 = FrmImagen(app).grid(row=0, column=1, sticky="nsew",padx=10,pady=10)
+frame2 = FrmImagen(app).grid(row=0, column=2, sticky="nsew",padx=10,pady=10)
+frame3 = FrmDatos(app).grid(row=1, column=0,columnspan=3, sticky="nsew",padx=10,pady=10)
 
 app.mainloop()
